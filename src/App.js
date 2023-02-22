f8ef877df1963f8eee3136489dfb7ecafcbde442
omargonza <gonzamartinez1081@gmail.com>
2023-02-22 11:52:17 -0300

    segundo commit

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import "./App.css"
+import { BrowserRouter, Routes, Route,Link } from "react-router-dom"
+import Header from "./components/common/header/Header"
+
+import About from "./components/about/About"
+import CourseHome from "./components/allcourses/CourseHome"
+import Team from "./components/team/Team"
+import Pricing from "./components/pricing/Pricing"
+import Blog from "./components/blog/Blog"
+import Contact from "./components/contact/Contact"
+import Footer from "./components/common/footer/Footer"
+import Home from "./components/home/Home"
+
+
+
+
+
+function App() {
+  return (
+ 
+   
+  <>
+        <Header />
+        <BrowserRouter>
+        <Routes>
+          <Route exact path='/' component={Home} />
+          <Route exact path='/about' component={About} />
+          <Route exact path='/courses' component={CourseHome} />
+          <Route exact path='/team' component={Team} />
+          <Route exact path='/pricing' component={Pricing} />
+          <Route exact path='/journal' component={Blog} />
+          <Route exact path='/contact' component={Contact} />
+        </Routes>
+        </BrowserRouter>
+        <Footer />
+       
+        
+   </>
+  
+  )
+}
+
+export default App
